52af731c03f2977a081f46b6761f5775ca4330f3
MadalenaCampos <madacampos02@gmail.com>
2022-04-28 17:38:37 -0300

    :sparkles: implementando logica do jogo

--- src/app/jogo-da-velha/jogo-da-velha.component.ts
+++ src/app/jogo-da-velha/jogo-da-velha.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { JogoDaVelhaService } from './shared';
 
 @Component({
   selector: 'app-jogo-da-velha',
   templateUrl: './jogo-da-velha.component.html',
-  styleUrls: ['./jogo-da-velha.component.less']
+  styleUrls: ['./jogo-da-velha.component.less'],
 })
 export class JogoDaVelhaComponent implements OnInit {
-
-  constructor() { }
+  constructor(private jogoDaVelhaService: JogoDaVelhaService) {}
 
   ngOnInit(): void {
+    this.jogoDaVelhaService.inicializar();
   }
 
+  // Definindo qual tela deve ser exibida.
+  get showInicio(): boolean {
+    return this.jogoDaVelhaService.showInicio;
+  }
+
+  get showTabuleiro(): boolean {
+    return this.jogoDaVelhaService.showTabuleiro;
+  }
+
+  get showFinal(): boolean {
+    return this.jogoDaVelhaService.showFinal;
+  }
+  // Definindo qual tela deve ser exibida.
+
+  public iniciarJogo(): void {
+    this.jogoDaVelhaService.iniciarJogo();
+  }
+
+  public jogar(posX: number, posY: number): void {
+    this.jogoDaVelhaService.jogar(posX, posY);
+  }
+
+  public exibirX(posX: number, posY: number): boolean {
+    return this.jogoDaVelhaService.exibirX(posX, posY);
+  }
+
+  public exibirO(posX: number, posY: number): boolean {
+    return this.jogoDaVelhaService.exibirO(posX, posY);
+  }
+
+  public exibirVitoria(posX: number, posY: number): boolean {
+    return this.jogoDaVelhaService.exibirVitoria(posX, posY);
+  }
+
+  get jogador(): number {
+    return this.jogoDaVelhaService.jogador;
+  }
+
+  public novoJogo(): void {
+    this.jogoDaVelhaService.novoJogo();
+  }
 }
