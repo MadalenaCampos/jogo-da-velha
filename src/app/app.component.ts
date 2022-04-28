4117d9f950b60c65d3eac7bd6364d48cec37e194
MadalenaCampos <madacampos02@gmail.com>
2022-04-28 11:39:13 -0300

    :bug: resolvendo problemas de uso de icone do zorro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'jogo-da-velha';
+  title = 'Jogo da velha #';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Importação de módulos do NG-ZORRO
+import { NzIconModule } from 'ng-zorro-antd/icon';
+
+// Solução para problema com ícones do NG-ZORRO
+import { IconDefinition } from '@ant-design/icons-angular';
+import {
+  AccountBookFill,
+  AlertFill,
+  AlertOutline,
+} from '@ant-design/icons-angular/icons';
+
+const icons: IconDefinition[] = [AccountBookFill, AlertOutline, AlertFill];
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, NzIconModule.forRoot(icons)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
